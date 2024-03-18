b453a6ff1d41956418ea39cc585107bc06fec6c4
Kevin Da Cruz <kevindacruz@Air-de-Lea.lan>
2024-03-18 16:41:57 +0100

    third-commit

--- src/components/DynamicParagraph.jsx
+++
@@ -1,5 +0,0 @@
-const DynamicParagraph = (props) => {
-  return <p>{props.paragraph}</p>;
-};
-
-export default DynamicParagraph;
--- src/components/DynamicTitle.jsx
+++
@@ -1,5 +0,0 @@
-const DynamicTitle = (props) => {
-  return <h1>{props.title}</h1>;
-};
-
-export default DynamicTitle;
